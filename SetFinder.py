223e283bdbe9f8e926a3d59eff164d19204948aa
Tobijlst <Tobiadijlst@gmail.com>
2025-01-15 14:22:02 +0100

    Update SetFinder.py

--- SetFinder.py
+++ SetFinder.py
@@ -1,20 +1,20 @@
 from itertools import combinations
 from SetClass import cardSet
 
-def isSet(card1,card2,card3): #functie om te checken of 3 kaarten een set zijn of niet
+def isSet(card1,card2,card3): # functie om te checken of 3 kaarten een set zijn of niet
     return ( (card1.number + card2.number + card3.number)%3 == 0 
             and (card1.color + card2.color + card3.color)%3 == 0 
             and (card1.shape + card2.shape + card3.shape)%3 == 0 
             and (card1.shade + card2.shade + card3.shade)%3 == 0    
         )
 
-def findOneSet(cards): #functie om 1 set te vinden
+def findOneSet(cards): # functie om 1 set te vinden
     for combo in combinations(cards, 3):
         if isSet(*combo):
             return combo
     return None
 
-def findAllSets(cards): #vind alle sets in een lijst (cards)
+def findAllSets(cards): # vind alle sets in een lijst (cards)
     sets = []
     for combo in combinations(cards, 3):
         if isSet(*combo):
@@ -23,7 +23,7 @@ def findAllSets(cards): #vind alle sets in een lijst (cards)
 
 def checkSelectedSet(cards):
     selected_cards = [card for card in cards if card.selected]
-    if len(selected_cards) == 3:
+    if len(selected_cards) == 3:    # Ensures the code doesn't crash
         if isSet(*selected_cards):  # Use the isSet function from SetFinder.py
             return True
     return False
